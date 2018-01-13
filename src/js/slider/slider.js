9abd90ace199998dbf72fcc5f84a8a5cd299da60
mrScrpt <dolcegabbana2010@gmail.com>
2018-01-13 17:49:15 +0200

    v1.9.9 +mainPage

--- src/js/slider/slider.js
+++ src/js/slider/slider.js
@@ -2,5 +2,41 @@ $('.hitProducts__inner').slick({
     infinite: false,
     speed: 300,
     slidesToShow: 4,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true
+
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 320,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
 });
